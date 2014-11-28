909309ea4f4c0acb0d83591ad2daf25215b43f8a
ToPresa <topresa16@hotmail.com>
2014-11-28 22:24:36 +0000

    tables

--- PHP/database.sql
+++ PHP/database.sql
@@ -1,8 +1,34 @@
+PRAGMA foreign_keys = on;
+
 CREATE TABLE account (
 	idAccount INTEGER PRIMARY KEY,
 	username NVARCHAR2(20) NOT NULL,
 	email NVARCHAR2(20) NOT NULL,
 	password NVARCHAR2(20) NOT NULL
+);
+
+CREATE TABLE poll(
+	idPoll INTEGER PRIMARY KEY,
+	title NVARCHAR2(20) NOT NULL,
+	image NVARCHAR2(20) NOT NULL,
+	public BOOLEAN,
+	accountId INTEGER,
+	FOREIGN KEY(accountId) REFERENCES account(idAccount)
+);
+
+CREATE TABLE question(
+	idQuestion INTEGER PRIMARY KEY,
+	quest NVARCHAR2(20) NOT NULL,
+	pollId INTEGER,
+	FOREIGN KEY(pollId) REFERENCES poll(idPoll)
+);
+
+CREATE TABLE answer(
+	idAnswer INTEGER PRIMARY KEY,
+	answ NVARCHAR2(20) NOT NULL,
+	votes INTEGER,
+	questionId INTEGER,
+	FOREIGN KEY(questionId) REFERENCES question(idQuestion)
 );
 
 ---- EXEMPLO DE CONTA ----
